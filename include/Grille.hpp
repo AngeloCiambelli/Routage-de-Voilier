b0c5ebee3355f918793cb9e3f7e63ce5d4eb40ec
AngeloCiambelli <ang.ciambelli@orange.fr>
2024-03-02 10:36:59 +0100

    correction interpolation

--- include/Grille.hpp
+++ include/Grille.hpp
@@ -10,6 +10,7 @@
 #include <sstream>
 #include <algorithm>
 #include "Vecteur.hpp"
+#include "Bi_vecteur.hpp"
 
 using namespace std;
 
@@ -83,39 +84,39 @@ Grille<T>::Grille(int X, int Y, int Time, float res, float p)
   valeur = tmp;
 };
 
-// template<typename T>
-// bi_vecteur<int> Grille<T>::localisation(const float &x, const float &y){
-//     // rend les quatres indices des points ABCD correspondant au rectangle ABCD
-//     // dans lequel se trouve le point de coordonnée (x,y) (A en bas gauche, D au-dessus de A)
-//     vecteur<int> Xs(4);
-//     vecteur<int> Ys(4);
-//     Xs[0] = int(floor(x/resolution));
-//     Ys[0] = int(floor(y/resolution));
-//     Xs[1] = Xs[0] + 1;
-//     Ys[1] = Ys[0];
-//     Xs[2] = Xs[1];
-//     Ys[2] = Ys[1] + 1;
-//     Xs[3] = Xs[0];
-//     Ys[3] = Ys[2];
-//     bi_vecteur<int> result(Xs,Ys);
-//     return result;
-// }
+template<typename T>
+bi_vecteur<int> Grille<T>::localisation(const float &x, const float &y){
+    // rend les quatres indices des points ABCD correspondant au rectangle ABCD
+    // dans lequel se trouve le point de coordonnée (x,y) (A en bas gauche, D au-dessus de A)
+    vecteur<int> Xs(4);
+    vecteur<int> Ys(4);
+    Xs[0] = int(floor(x/resolution));
+    Ys[0] = int(floor(y/resolution));
+    Xs[1] = Xs[0] + 1;
+    Ys[1] = Ys[0];
+    Xs[2] = Xs[1];
+    Ys[2] = Ys[1] + 1;
+    Xs[3] = Xs[0];
+    Ys[3] = Ys[2];
+    bi_vecteur<int> result(Xs,Ys);
+    return result;
+}
 
-// template<typename T>
-// T Grille<T>::interpolation(const float &x, const float &y, const int &timestamp){
-//   T sum = T();
-//   bi_vecteur position = localisation(const float &x, const float &y);
-//   float w11;float w12;float w21; float w22;
-//   float x1;float x2;float y1; float y2;
-//   x1 = position.X[0];x2 = position.X[2];
-//   y1 = position.Y[0];y2 = position.Y[2];
-//   float D = (x2-x1)(y2-y1);
-//   w11 = (x2 - x)(y2 - y)/D;
-//   w12 = (x2 - x)(y - y1)/D;
-//   w21 = (x - x1)(y2 - y)/D;
-//   w22 = (x - x1)(y - y1)/D;
-//   sum = valeur[timestamp][y1][x1]*w11 + valeur[timestamp][y2][x1]*w21 + valeur[timestamp][y1][x2]*w12 + valeur[timestamp][y2][x2]*w22;
-//   return sum;
-// };
+template<typename T>
+T Grille<T>::interpolation(const float &x, const float &y, const int &timestamp){
+  T sum = T();
+  bi_vecteur position = localisation(x,y);
+  float w11;float w12;float w21; float w22;
+  float x1;float x2;float y1; float y2;
+  x1 = position.X[0];x2 = position.X[2];
+  y1 = position.Y[0];y2 = position.Y[2];
+  float D = (x2-x1)*(y2-y1);
+  w11 = (x2 - x)*(y2 - y)/D;
+  w12 = (x2 - x)*(y - y1)/D;
+  w21 = (x - x1)*(y2 - y)/D;
+  w22 = (x - x1)*(y - y1)/D;
+  sum = valeur[timestamp][y1][x1]*w11 + valeur[timestamp][y2][x1]*w21 + valeur[timestamp][y1][x2]*w12 + valeur[timestamp][y2][x2]*w22;
+  return sum;
+};
 
 #endif
