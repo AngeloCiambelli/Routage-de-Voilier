45c6620b3cdcf2bd1d9210bc30b687745c19c0e7
YanisJouanaud <yanis.jouanaud@gmail.com>
2024-03-03 20:22:08 +0100

    Correction interpolation

--- include/Fonction_externes.hpp
+++ include/Fonction_externes.hpp
@@ -24,14 +24,16 @@ T1 interpolation(const bi_vecteur<int> position, const int &timestamp, const T2 
   int x1 = position.X[0];int x2 = position.X[2];
   int y1 = position.Y[0];int y2 = position.Y[2];
   float D = (x2-x1)*(y2-y1);
-  w11 = (x2 - x)*(y2 - y)/D;
-  w12 = (x2 - x)*(y - y1)/D;
-  w21 = (x - x1)*(y2 - y)/D;
-  w22 = (x - x1)*(y - y1)/D;
+  int w11 = (x2 - x)*(y2 - y)/D;
+  int w12 = (x2 - x)*(y - y1)/D;
+  int w21 = (x - x1)*(y2 - y)/D;
+  int w22 = (x - x1)*(y - y1)/D;
   sum = valeur[grille.find(x1,y1,timestamp)]*w11 + valeur[grille.find(x1,y2,timestamp)]*w21 + valeur[grille.find(x2,y1,timestamp)]*w12 + valeur[grille.find(x2,y2,timestamp)]*w22;
   return sum;
+};
 
-}
+
+
 
 template<typename T>
 vecteur<pair<vecteur<vecteur<T>>, vecteur<vecteur<T>>>> bi_vecteur_vers_table(const bi_vecteur<T>& v,const Grille& grille)
