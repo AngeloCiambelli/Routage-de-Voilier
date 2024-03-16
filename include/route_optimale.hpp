078ad4473ac113c3880f4091879e3812d49d1035
AngeloCiambelli <ang.ciambelli@orange.fr>
2024-03-16 14:27:26 +0100

    fixed bug

--- include/route_optimale.hpp
+++ include/route_optimale.hpp
@@ -23,21 +23,21 @@ class route_optimale{
     Grille grille;
     int max_iter = 1000;
     Dynamique_voile vitesse;
-    HJB HJB;
+    HJB HJB_new;
 
     public:
     Bi_vecteur<float> positions;
     int n;
 
     route_optimale(Grille& g, Dynamique_voile& f, Vecteur<float>& v0):  // constructeur par défaut
-    grille(g), vitesse(f), HJB(v0,g,f), max_iter(grille.Temps/grille.resolution), positions(){}
+    grille(g), vitesse(f), HJB_new(v0,g,f), max_iter(grille.Temps/grille.resolution), positions(){}
 
     int calcul_n(const Vecteur<float> &x0){ // calcul du nombre d'itération requise
         float v0k = 1.f;
         int i = 0;
         Bi_vecteur<int> loca = grille.localisation(x0[0], x0[1]);  // localisation du point de départ dans la grille
         while(i<=max_iter && v0k>0){  // on cherche la première occurence n de v_n telle que le point de départ soit négatif
-            v0k = interpolation<float, Vecteur<float>>(loca, x0[0], x0[1], i, HJB.v, grille);
+            v0k = interpolation<float, Vecteur<float>>(loca, x0[0], x0[1], i, HJB_new.v, grille);
             i++;
         }
         n=i-1; // n est la dernière occurence où v(x0, n) est positive
@@ -48,10 +48,10 @@ class route_optimale{
     commandes_discretes calcul(Vecteur<float>& x0, int L){
         positions.X.clear();
         positions.Y.clear();
-        if(HJB.resolved==0){HJB.resolve(L);} // On résout HJB si il n'a jamais été résolu, sinon on peut procédé
-        // print_grille(grille, HJB.v);
+        if(HJB_new.resolved==0){HJB_new.resolve(L);} // On résout HJB si il n'a jamais été résolu, sinon on peut procédé
+        // print_grille(grille, HJB_new.v);
         Bi_vecteur<int> loca = grille.localisation(x0[0], x0[1]); // localisation du point de départ dans la grille
-        float vx = interpolation<float, Vecteur<float>>(loca, x0[0], x0[1], 0, HJB.v, grille); // interpolation sur v du point de départ
+        float vx = interpolation<float, Vecteur<float>>(loca, x0[0], x0[1], 0, HJB_new.v, grille); // interpolation sur v du point de départ
         int iter = 0;
         n = calcul_n(x0);  //calcul du nombre d'iteration nécessaire
 
@@ -70,7 +70,7 @@ class route_optimale{
                 loca = grille.localisation(next_pos[0], next_pos[1]); // prochaine localisation dans la grille
                 if(loca.X[0]>=0 && loca.Y[0]>=0 && loca.X[2]<=int(grille.taille_X/grille.pas) && loca.Y[2]<=int(grille.taille_Y/grille.pas)){
                     // si la commande ne nous amène pas en dehors de la zone d'étude
-                    minimiseur = interpolation<float, Vecteur<float>>(loca, next_pos[0], next_pos[1], n-iter, HJB.v, grille);
+                    minimiseur = interpolation<float, Vecteur<float>>(loca, next_pos[0], next_pos[1], n-iter, HJB_new.v, grille);
                     if(minimiseur<mini){
                         mini=minimiseur;
                         commande=float(l)/float(L)*360.f-180.f;
@@ -90,7 +90,7 @@ class route_optimale{
             positions.pushback(opti_pos);
             commandes.commandes.push_back(commande);
             loca = grille.localisation(opti_pos[0], opti_pos[1]);
-            vx = interpolation<float, Vecteur<float>>(loca, opti_pos[0], opti_pos[1], 0, HJB.v, grille); //nouvelle valeur de v en la position considéré
+            vx = interpolation<float, Vecteur<float>>(loca, opti_pos[0], opti_pos[1], 0, HJB_new.v, grille); //nouvelle valeur de v en la position considéré
             // Pour rappel, on veut qu'elle soit négative, ce qui signifie qu'on est arrivé dans la zone cible
             iter++;
         }
